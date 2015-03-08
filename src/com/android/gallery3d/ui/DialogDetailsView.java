d52e6fdd69c73f4f2d3996618ae005d2a2c05afb
maxwen <max.weninger@gmail.com>
2015-03-08 23:36:49 +0100

    Gallery2: ignore null detail lines
    dont exit if details contains empty value lines
    just ignore them.
    
    Change-Id: I3b07a6f4f7e591f8e21dabef4d39cf693200e5b2

--- src/com/android/gallery3d/ui/DialogDetailsView.java
+++ src/com/android/gallery3d/ui/DialogDetailsView.java
@@ -225,8 +225,10 @@ public class DialogDetailsView implements DetailsViewContainer {
                         Object valueObj = detail.getValue();
                         // This shouldn't happen, log its key to help us diagnose the problem.
                         if (valueObj == null) {
-                            Utils.fail("%s's value is Null",
+                            Utils.debug("%s's value is Null",
                                     DetailsHelper.getDetailsName(context, detail.getKey()));
+                            // ignore this detail
+                            continue;
                         }
                         value = valueObj.toString();
                     }
