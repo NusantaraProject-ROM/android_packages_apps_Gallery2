47eeaa81984459a56586ce6a5dd14aa69d8db6d3
Stefan Berger <s.berger81@gmail.com>
2017-11-26 14:07:31 +0100

    Gallery2: jni: Remove -ffast-math from CFLAGS
    Fixes CLANG build breakage for certain archs
    
    Change-Id: I1175320a7d2c07557d437d1c599f90686146bb0d

--- jni/Android.mk
+++ jni/Android.mk
@@ -44,7 +44,7 @@ LOCAL_SRC_FILES := filters/gradient.c \
                    filters/tinyplanet.cc \
                    filters/kmeans.cc
 
-LOCAL_CFLAGS    += -ffast-math -O3 -funroll-loops -Wno-unused-parameter
+LOCAL_CFLAGS    += -O3 -funroll-loops -Wno-unused-parameter
 LOCAL_LDLIBS := -llog -ljnigraphics
 LOCAL_ARM_MODE := arm
 
