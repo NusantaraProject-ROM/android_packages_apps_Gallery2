d3222e5e476805d96fcbab6cddbe2a83dcf954fb
maxwen <max.weninger@gmail.com>
2018-08-10 13:47:38 +0200

    Gallery2: Pie resurrection
    Change-Id: Iff371412a87c0ef28f97b1d1035073a644805eda

--- Android.mk
+++ Android.mk
@@ -4,19 +4,25 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
+LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v4 \
     android-support-v13
-LOCAL_STATIC_JAVA_LIBRARIES += com.android.gallery3d.common2
-LOCAL_STATIC_JAVA_LIBRARIES += xmp_toolkit
-LOCAL_STATIC_JAVA_LIBRARIES += mp4parser
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-renderscript-files-under, src)
-LOCAL_SRC_FILES += $(call all-java-files-under, src_pd)
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    com.android.gallery3d.common2 \
+    xmp_toolkit \
+    mp4parser
+
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, src) \
+    $(call all-renderscript-files-under, src) \
+    $(call all-java-files-under, src_pd)
 
 LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay
+
+LOCAL_USE_AAPT2 := true
 
 LOCAL_PACKAGE_NAME := Gallery2
 
@@ -27,17 +33,13 @@ LOCAL_OVERRIDES_PACKAGES := Gallery Gallery3D GalleryNew3D
 LOCAL_SDK_VERSION := current
 LOCAL_MIN_SDK_VERSION := 24
 
-LOCAL_JNI_SHARED_LIBRARIES := libjni_eglfence libjni_filtershow_filters librsjni libjni_jpegstream
+LOCAL_JNI_SHARED_LIBRARIES := \
+    libjni_eglfence \
+    libjni_filtershow_filters \
+    libjni_jpegstream
 
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+LOCAL_PROGUARD_ENABLED := disabled
 
-LOCAL_JAVA_LIBRARIES += org.apache.http.legacy
+LOCAL_JAVA_LIBRARIES := org.apache.http.legacy
 
 include $(BUILD_PACKAGE)
-
-ifeq ($(strip $(LOCAL_PACKAGE_OVERRIDES)),)
-
-# Use the following include to make gallery test apk
-include $(call all-makefiles-under, $(LOCAL_PATH))
-
-endif
